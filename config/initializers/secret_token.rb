0061c311be4264ddf65b16fe7eeca6df8cc94087
笠井 暁史 <SK-macbook@kasai-no-MacBook-Air.local>
2013-08-04 11:05:01 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Healthya::Application.config.secret_key_base = '8816dfc1a50598bbf7f26b01069f26024851c0434f4d70e40e4f02203a9ace7cae37e0ea7fa0e1721dda4fc1a983cfbf295e73a4d2a29a119adb0b576b5a1bed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Healthya::Application.config.session_store :cookie_store, key: '_healthya_session'
